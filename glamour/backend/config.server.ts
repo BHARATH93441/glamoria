66465364b639410358f079b06963fd550686c285
Vishal6374 <vishalmurugavel105@gmail.com>
2026-06-16 07:51:41 +0530

    env updated

--- glamour/backend/config.server.ts
+++
@@ -1,9 +0,0 @@
-import process from "node:process";
-
-export function getServerConfig() {
-  return {
-    nodeEnv: process.env.NODE_ENV,
-    mongodbUri: process.env.MONGODB_URI,
-    mongodbDb: process.env.MONGODB_DB,
-  };
-}
